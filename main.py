38bbcc3da685765b782e05ad80a42819364c84f9
Marco Dominguez <marcod082@gmail.com>
2024-01-03 14:03:30 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from pypdf import PdfReader
+import pyttsx3
+
+#create PdfReader object
+reader = PdfReader("meta-pdf.pdf")
+
+#create a pyttsx3 object
+engine = pyttsx3.init()
+
+#obtain the speech rate value and set a new rate
+rate = engine.getProperty("rate")
+engine.setProperty("rate", rate + 50)
+
+#iterate thru the pages of pdf and append the text from e/ page to a string
+pdf_text = ""
+for page in reader.pages:
+    pdf_text += page.extract_text()
+    
+#save text to mp3 file 
+engine.save_to_file(pdf_text, "tts_pdf.mp3")
+engine.runAndWait() #execute all queued engine commands
